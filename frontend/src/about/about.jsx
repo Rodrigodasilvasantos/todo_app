ff44c511eab4d9460609049890b089a53092a5c4
Rodrigodasilvasantos <44823505+Rodrigodasilvasantos@users.noreply.github.com>
2020-05-21 12:49:04 -0300

    Templates / rotas

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small= 'Nós'></PageHeader>
+        <h2>História</h2>
+        <p>Lorem ipsum</p>
+        
+    </div>
+)
